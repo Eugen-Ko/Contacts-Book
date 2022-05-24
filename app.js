f01a6e015b2293b5c61fa60909dc9dbafb9f0bfd
Eugen <kovalchuk.eak@gmail.com>
2022-05-24 21:51:34 +0300

    start

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
 const express = require("express");
 const logger = require("morgan");
 const cors = require("cors");
+const mongoose = require("mongoose");
 
 const contactsRouter = require("./routes/api/contacts");
+
+require("dotenv").config();
+const { DB_HOST } = process.env;
+
+mongoose
+  .connect(DB_HOST)
+  .then(() => console.log("Database connect successful ..."))
+  .catch((error) => {
+    console.log(error.message);
+    process.exit(1);
+  });
 
 const app = express();
 
@@ -14,11 +26,11 @@ app.use(express.json());
 
 app.use("/api/contacts", contactsRouter);
 
-app.use((req, res) => {
+app.use((_, res) => {
   res.status(404).json({ message: "Not found" });
 });
 
-app.use((err, req, res, next) => {
+app.use((err, _, res, __) => {
   const { status = 500, message = "Sever error" } = err;
   res.status(status).json({
     status: "error",
