6e561590e8a006e696990be95e1b82e934706187
Eugen <kovalchuk.eak@gmail.com>
2022-05-25 13:01:20 +0300

    Update removeById.js

--- controllers/contacts/removeById.js
+++ controllers/contacts/removeById.js
@@ -4,9 +4,6 @@ const { Contact } = require("../../models");
 
 const removeById = async (req, res) => {
   const { id } = req.params;
-  if (id.length !== 24) {
-    throw new NotFound(`Contacts with id=${id} not found`);
-  }
   const result = await Contact.findByIdAndRemove(id);
   if (!result) throw new NotFound(`Contacts with id=${id} not found`);
   res.status(201).json({
